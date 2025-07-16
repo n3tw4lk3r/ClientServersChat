ad8076836db0ae46c2b053fd1b72f403e5b64962
n3tw4lk3r <piroman.pasha@yandex.ru>
2025-07-16 19:29:22 +0300

    Introduce Client

---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        using TcpClient client = new TcpClient("127.0.0.1", 5000);
+        using NetworkStream stream = client.GetStream();
+
+        Console.WriteLine("Enter your message. Press Ctrl-C to quit.");
+
+        while (true)
+        {
+            Console.Write("> ");
+            string? message = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(message))
+                continue;
+
+            byte[] data = Encoding.UTF8.GetBytes(message + "\n");
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
